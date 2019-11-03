582ebfa31b4ad9b62f0b6f32c96a84ad01e6fb4b
James <admin@James.local>
2019-11-03 17:14:52 +0300

    initial commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://james-beamie:mern-beamie@mern-shop-nbjft.mongodb.net/test?retryWrites=true&w=majority"
+};
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const db = require("./config/keys").mongoURI;
+
+const app = express();
+
+// Body parser middleware
+app.use(bodyParser.json());
+
+mongoose
+  .connect(db)
+  .then(() => {
+    console.log("connected");
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`server running on port ${port}`));
